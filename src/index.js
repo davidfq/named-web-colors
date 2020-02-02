6d0a23b9675362b2f9df5425b1d99bfbe5a3c59f
David <davidfq@gmail.com>
2020-02-02 12:07:25 +0100

    Improve general implementation: new output object color spec
    - API changes: options object instead of params
    - Better alpha channel handling
    - New output object color for most common use-case
    - Update test cases

--- src/index.js
+++ src/index.js
@@ -1,23 +1,33 @@
 import colorString from 'color-string'
-import colors from './colors'
 
-const colorList = Object.keys(colors).map((key) => {
-  const color = colorString.get(`#${key}`)
-  return {
-    hex: key,
-    name: colors[key],
-    rgb: color.value
-  }
-})
+import colorsCurated from '../data/curated.json'
+import colorsWeb from '../data/web.json'
+import colorsWerner from '../data/werner.json'
+
+const WHITE = colorString.get('#fff')
+const BLACK = colorString.get('#000')
 
 /**
+ * Describes a matched color.
+ *
+ * @typedef {Object} ColorOutput
+ * @property {string} name - The name of the matched color, e.g., 'red'
+ * @property {string} hex - Hex color code e.g., '#FF0'
+ * @property {string} rgb - RGB definition (or RGBA for colors with alpha channel).
+ * @property {string} css - CSS custom property alike definition, e.g.
+ *  `--color-prussian-blue: #004162`
+ * @property {number} distance - Calculated distance between input and matched color.
+ */
+
+/**
+ * Square root of sum of the squares of the differences in values
+ * [red, green, blue, opacity]
+ *
  * @param {Array} color1
  * @param {Array} color2
- * @return {number}
+ * @return {Number}
  */
 const euclideanDistance = (color1, color2) => {
-  // square root of sum of the squares of the differences in values
-  // [red, green, blue, opacity]
   return Math.sqrt(
     Math.pow(color1[0] - color2[0], 2) +
     Math.pow(color1[1] - color2[1], 2) +
@@ -25,85 +35,174 @@ const euclideanDistance = (color1, color2) => {
   )
 }
 
-const white = colorString.get('#fff')
-const black = colorString.get('#000')
-const MAX_DISTANCE = euclideanDistance(white.value, black.value)
+const MAX_DISTANCE = euclideanDistance(WHITE.value, BLACK.value)
 
 /**
- * Super basic impl, it's only expected to work with strings coming from `colorList`
- * @param {string} string
- * @return {string}
- */
-const slugify = (string = '', separator = '-') => {
-  const chars = string.trim().split('')
-  return chars.reduce((memo, char) => {
-    return memo + char.replace(/'/, '').replace(/\s/, separator)
-  }, '').toLocaleLowerCase()
-}
-
-/**
- * Combines "color" with solid white, it tries to get a new solid color when
- * "color" has some transparency value `color[3] < 1`
+ * Combines foreground and background colors.
  * ref: https://en.wikipedia.org/wiki/Transparency_%28graphic%29
- * @param {Array} color - [red, green, blue, alpha]
+ *
+ * @param {Array} foreground - [red, green, blue, alpha]
+ * @param {Array} background - [red, green, blue, alpha]
  * @return {Array} - [red, green, blue, alpha=1]
  */
-const combineWithWhite = (color) => {
-  const opacity = color[3]
+const blend = (foreground, background) => {
+  const opacity = foreground[3]
   return [
-    ((1 - opacity) * white.value[0]) + (opacity * color[0]),
-    ((1 - opacity) * white.value[1]) + (opacity * color[1]),
-    ((1 - opacity) * white.value[2]) + (opacity * color[2]),
+    ((1 - opacity) * background[0]) + (opacity * foreground[0]),
+    ((1 - opacity) * background[1]) + (opacity * foreground[1]),
+    ((1 - opacity) * background[2]) + (opacity * foreground[2]),
     1
   ]
 }
 
 /**
- * Main function exported
- * @param {string} colorCode Code representing the color to translate to
- * @param {boolean} slug Whether to "slugify" the result or not
- * @param {boolean} opaque Whether to combine input color with white background
+ * Calculates color distance based on whether first param color input has
+ * alpha channel or not.
+ *
+ * @param {Array} color1
+ * @param {Array} color2
+ * @return {number}
+ */
+const comparativeDistance = (color1, color2) => {
+  if (color1[3] === 1 && color2[3] === 1) {
+    // solid colors: use basic Euclidean distance algorithm
+    return euclideanDistance(color1, color2)
+  } else {
+    // alpha channel: combine input color with white and black backgrounds
+    // and comparte distances
+    const withWhite = euclideanDistance(blend(color1, WHITE.value), color2)
+    const withBlack = euclideanDistance(blend(color1, BLACK.value), color2)
+    return withWhite <= withBlack ? withWhite : withBlack
+  }
+}
+
+/**
+ * Transform color name to web-safe slug.
+ *
+ * @param {string} string
  * @return {string}
  */
-export default function getColorName (colorCode, slug = false, opaque = true) {
-  const inputColor = colorString.get(colorCode)
+const slugify = (string = '', separator = '-') => {
+  return string.trim().split('').reduce((memo, char) => {
+    return memo + char.replace(/'/, '').replace(/\s/, separator)
+  }, '').toLocaleLowerCase()
+}
+
+/**
+ * Simple RGB comparation method.
+ *
+ * @param {Array} color1
+ * @param {Array} color2
+ * @param {boolean} ignoreAlphaChannel
+ */
+const compareRGB = (color1, color2, ignoreAlphaChannel = false) => {
+  let result = false
+  if (color1.length === 4 && color2.length === 4) {
+    result = color1[0] === color2[0] &&
+      color1[1] === color2[1] &&
+      color1[2] === color2[2]
+
+    result = ignoreAlphaChannel ? result : color1[3] === color2[3]
+  }
+  return result
+}
+
+/**
+ * Build output color object spec.
+ *
+ * @param {string} name - resolved color name
+ * @param {string} hex - Hex color code
+ * @param {Object} colorInput
+ * @param {number} distance
+ * @param {boolean} ignoreAlphaChannel
+ * @return {ColorOutput}
+ */
+const buildColorOutput = (name, hex, colorInput, distance, ignoreAlphaChannel) => {
+  const alpha = colorInput.value[3]
+  let result = {
+    name: name,
+    distance: distance
+  }
+
+  if (ignoreAlphaChannel) {
+    result.hex = `#${hex}`
+    result.css = `--color-${slugify(name)}: ${result.hex}`
+  } else {
+    // use HEX code from input directly as any of the curated colors
+    // don't have alpha channel defined
+    result.hex = colorString.to.hex(colorInput.value)
+    result.css = (alpha < 1)
+      ? `--color-${slugify(name)}-${(alpha * 100)}: ${result.hex}`
+      : `--color-${slugify(name)}: ${result.hex}`
+  }
+
+  // double check final result; `color-string` don't support HSL input
+  // transforms to HEX (when input contains decimals, @see tests),
+  // so `result.hex` may not be valid at this point
+  if (colorString.get(result.hex) !== null) {
+    result.rgb = colorString.to.rgb(colorString.get(result.hex).value)
+  } else {
+    result = undefined
+  }
+
+  return result
+}
+
+/**
+ * Main function to find the closest color among "colors" list.
+ *
+ * @param {string} code - color code: Hex, RGB or HSL
+ * @param {Object} colors - color list: keys are Hex codes and values are color names
+ * @param {boolean} ignoreAlphaChannel - whether to ignore alpha channel on input
+ */
+const getColor = (code, colors, ignoreAlphaChannel = false) => {
+  const colorInput = colorString.get(code)
+  const colorKeys = Object.keys(colors)
+  let colorKeyMatch
   let distance = MAX_DISTANCE
-  let colorMatch = {}
+  let result
 
-  // is it a web color?
-  if (inputColor !== null) {
-    const webColor = colorString.to.keyword(inputColor.value)
-    if (webColor !== undefined) {
-      colorMatch = {
-        name: webColor
-      }
+  if (colorInput !== null) {
+    // check if there's an exact match (it only happens with solid colors)
+    if (!ignoreAlphaChannel) {
+      colorKeyMatch = colorKeys.find((key) => {
+        const color = colorString.get(`#${key}`)
+        return compareRGB(color.value, colorInput.value, true)
+      })
+    }
+
+    if (colorKeyMatch !== undefined) {
+      distance = 0
+    }
+
+    if (distance > 0) {
+      // let's find the closest one
+      const calculateDistance = ignoreAlphaChannel ? comparativeDistance : euclideanDistance
+      colorKeys.forEach((key) => {
+        const colorCandidate = colorString.get(`#${key}`)
+        const tmpDistance = calculateDistance(colorInput.value, colorCandidate.value)
+        if (tmpDistance < distance) {
+          colorKeyMatch = key
+          distance = tmpDistance
+        }
+      })
     }
   }
 
-  // is it an exact match?
-  if (colorMatch.name === undefined && inputColor !== null) {
-    colorMatch = colorList.find((color) => {
-      return color.rgb === inputColor.value
-    }) || {}
+  if (colorKeyMatch !== undefined) {
+    result = buildColorOutput(colors[colorKeyMatch], colorKeyMatch, colorInput, distance,
+      ignoreAlphaChannel)
   }
 
-  // let's find the closest one
-  if (colorMatch.name === undefined && inputColor !== null) {
-    // is it transparent? [r, g, b, a]
-    const alpha = inputColor.value[3]
-    if (alpha < 1 && opaque) {
-      inputColor.value = combineWithWhite(inputColor.value)
-    }
+  return result
+}
 
-    colorList.forEach((color) => {
-      const tmpDistance = euclideanDistance(inputColor.value, color.rgb)
-      if (tmpDistance < distance) {
-        distance = tmpDistance
-        colorMatch = color
-      }
-    })
+export default function getColorName (code, options = {}) {
+  let colors = Object.assign({}, colorsWeb, colorsCurated)
+  if (options.list && options.list === 'web') {
+    colors = colorsWeb
+  } else if (options.list && options.list === 'werner') {
+    colors = colorsWerner
   }
-
-  const colorName = colorMatch.name || ''
-  return slug ? slugify(colorName) : colorName
+  return getColor(code, colors, options.ignoreAlphaChannel)
 }
--- test/index.js
+++ test/index.js
@@ -2,34 +2,52 @@ import test from 'ava'
 import getColorName from '../lib/named-web-colors'
 
 test('it doesn\'t break because of parsing errors', t => {
-  t.is(getColorName('#'), '')
-  t.is(getColorName('test'), '')
+  t.is(getColorName('#'), undefined)
+  t.is(getColorName('test'), undefined)
 })
 
 test('web color matches', t => {
-  // `color-string` leads with these
-  // https://lists.w3.org/Archives/Public/www-style/2014Jun/0312.html
-  t.is(getColorName('#663399'), 'rebeccapurple')
+  const color = getColorName('#663399')
+  t.is(color.css, '--color-rebecca-purple: #663399')
+  t.is(color.distance, 0)
 })
 
 test('Exact matches vs. variations', t => {
-  t.is(getColorName('#0D1117'), 'Bunker') // exact match
-  t.is(getColorName('#0D1118'), 'Bunker')
-  t.is(getColorName('#0D1116'), 'Bunker')
+  t.is(getColorName('#0D1117').name, 'Bunker') // exact match
+  t.is(getColorName('#0D1117').distance, 0) // exact match
+  t.is(getColorName('#0D1118').name, 'Bunker')
+  t.not(getColorName('#0D1118').distance, 0)
 })
 
 test('not only HEX supported', t => {
-  t.is(getColorName('rgb(13,17,23)'), 'Bunker')
-})
-
-test('web safe option (slug style output)', t => {
-  t.is(getColorName('#FFB97B', true), 'macaroni-and-cheese')
-  t.is(getColorName('#66FF66', true), 'screamin-green')
+  t.is(getColorName('#0D1117').name, 'Bunker')
+  t.is(getColorName('rgba(13, 17, 23, 1)').name, 'Bunker')
+  t.is(getColorName('rgb(13, 17, 23)').name, 'Bunker')
+  t.is(getColorName('hsl(216, 27%, 7%)').name, 'Thunderbird')
+  t.is(getColorName('hsl(216, 27.8%, 7.1%)'), undefined)
 })
 
 test('HEX opacity', t => {
-  t.is(getColorName('#004162FF', true), 'astronaut-blue') // fully opaque
-  t.is(getColorName('#00416280', true), 'bali-hai') // 50% - 80 alpha
-  t.is(getColorName('#0041621A', true), 'mystic') // 10% - 1A alpha
-  t.is(getColorName('#00416280', true, false), 'astronaut-blue') // ignore alpha
+  // fully opaque
+  t.is(getColorName('#004162FF').css, '--color-astronaut-blue: #004162')
+  // 50% - 80 alpha
+  t.is(getColorName('#00416280').css, '--color-astronaut-blue-50: #00416280')
+  // 10% - 1A alpha
+  t.is(getColorName('#0041621A').css, '--color-astronaut-blue-10: #0041621A')
+})
+
+test('Werner\'s nomenclature', t => {
+  t.is(getColorName('#004162FF', { list: 'werner' }).css,
+    '--color-prussian-blue: #004162')
+  t.is(getColorName('#00416280', { list: 'werner' }).css,
+    '--color-prussian-blue-50: #00416280')
+})
+
+test('ignore alpha channel', t => {
+  t.is(getColorName('#FFFFB440').css, '--color-portafino-25: #FFFFB440')
+  t.is(getColorName('#FFFFB440').rgb, 'rgba(255, 255, 180, 0.25)')
+  t.is(getColorName('#FFFFB440', { ignoreAlphaChannel: true }).css,
+    '--color-apricot-white: #FFFEEC')
+  t.is(getColorName('#FFFFB440', { ignoreAlphaChannel: true }).rgb,
+    'rgb(255, 254, 236)')
 })
