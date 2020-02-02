1302f4968f4bd2d8f47584a9ccf031c8985b6720
David <davidfq@gmail.com>
2020-02-02 10:49:45 +0100

    Update webpack config and dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,25 @@
+const webpack = require('webpack')
 const path = require('path')
+const info = require('./package.json')
+const banner = `${info.name} v${info.version}\n${info.homepage}`
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'lib'),
     filename: 'named-web-colors.js',
-    library: 'namedWebColors',
+    library: 'getColorName',
     libraryTarget: 'umd',
     libraryExport: 'default',
     umdNamedDefine: true,
     globalObject: "typeof self !== 'undefined' ? self : this"
-  }
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
+    ]
+  },
+  plugins: [
+    new webpack.BannerPlugin(banner)
+  ]
 }
